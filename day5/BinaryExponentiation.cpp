d0197b5e3a728678beba5d924fc932bcb11477e4
vsaa-org <23mcce16@uohyd.ac.in>
2025-05-31 23:13:50 +0530

    Improved code

--- day5/BinaryExponentiation.cpp
+++ day5/BinaryExponentiation.cpp
@@ -2,24 +2,34 @@
 
 using namespace std;
 
+double myPow(double x, int n) 
+{
+        
+        long binForm = n;
+        if(x == 0) return 0.0;
+
+        if(binForm < 0)
+        {
+            x = 1/x;
+            binForm *= -1;
+        }
+        double ans = 1;
+        while(binForm > 0)
+        {
+            if(binForm%2 == 1)
+                ans *= x;
+            
+            x *= x;
+            binForm /= 2;
+        }
+
+        return ans;
+}
+
 int main()
 {
     //Find x^n but with O(logn)
 
-    int x = 5;
-    int n = 10;
-    double ans = 1;
-
-    while(n > 0)
-    {
-        if(n%2 == 1)
-        {
-            ans = ans*x;
-        }
-        x *= x;
-        n /= 2;
-    }
-
-    cout << ans << " " << x;
+    cout << myPow(5, -1000);
     return 0;
 }
