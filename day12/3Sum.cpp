2da739c816755acf9715954168625b3a6e12c3d5
vsaa-org <23mcce16@uohyd.ac.in>
2025-06-15 00:01:24 +0530

    best solution for 3 sum

--- day12/3Sum.cpp
+++ day12/3Sum.cpp
@@ -36,31 +36,60 @@ vector<vector<int>> threeSum(vector<int> vec, int target)
     // return ans;
 
     //More optimized as time complexity -> O(n*n*log(unique triplets))
+    // for(int i=0;i<vec.size();i++)
+    // {
+    //     int a = -vec[i];
+    //     set<int> unique;
+    //     for(int j=i+1;j<vec.size();j++)
+    //     {
+    //         int b = vec[j];
+    //         int c = a - b;
+
+    //         if(unique.find(c) != unique.end())
+    //         {
+    //             vector<int> v = {-a, b, c};
+
+    //             sort(v.begin(), v.end());
+
+    //             if(s.find(v) == s.end())
+    //             {
+    //                 s.insert(v);
+    //                 ans.push_back(v);
+    //             }
+    //         }
+    //         else
+    //         {
+    //             unique.insert(b);
+    //         }
+    //     }
+    // }
+    // return ans;
+
+    //Best optimization using two pointer
+    //time complexity - O(logn + n*n)
+    sort(vec.begin(), vec.end());
     for(int i=0;i<vec.size();i++)
     {
-        int a = -vec[i];
-        set<int> unique;
-        for(int j=i+1;j<vec.size();j++)
+        if(i > 0 && vec[i] == vec[i-1]) continue;
+        int j=i+1, k=vec.size()-1;
+
+        while(j < k)
         {
-            int b = vec[j];
-            int c = a - b;
-
-            if(unique.find(c) != unique.end())
+            int sum = vec[i]+vec[j]+vec[k];
+            if(sum == 0)
             {
-                vector<int> v = {-a, b, c};
+                vector<int> v = {vec[i],vec[j],vec[k]};
 
-                sort(v.begin(), v.end());
-
-                if(s.find(v) == s.end())
-                {
-                    s.insert(v);
-                    ans.push_back(v);
-                }
+                ans.push_back(v);
+                j++;    k--;
+                while(j<k && vec[j] == vec[j+1])    j++;
+            }
+            else if(sum < 0)
+            {
+                j++;
             }
             else
-            {
-                unique.insert(b);
-            }
+                k--;
         }
     }
     return ans;
