37ea15070ad659eef2503a86d5acce25d3962369
vsaa-org <23mcce16@uohyd.ac.in>
2025-06-18 11:40:47 +0530

    String Permutation

---
+++ day14/StringPermutation.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+//Total number of permutation - n!
+//leetcode - 46
+//time complexity - O(n!*n)(total ans * how we reach)
+//space complexity - O(n! + n) -> O(n!)
+void printPermutation(string s, int pos)
+{
+    if(pos == s.size())
+    {
+        for(char ele : s)
+            cout << ele << " ";
+        cout << endl;
+        return ;
+    }
+
+    for(int i=pos;i<s.size();i++)
+    {
+        swap(s[i], s[pos]);
+        printPermutation(s, pos+1);
+        //backtracking
+        swap(s[i], s[pos]);
+    }
+}
+
+void permutation(string s)
+{
+    printPermutation(s, 0);
+}
+
+int main()
+{
+    string s = "abc";
+
+    permutation(s);
+    return 0;
+}
--- day14/readme.txt
+++ day14/readme.txt
@@ -2,3 +2,4 @@ Day 14:
     1. print all subsets(leetcode - 78)
     2. SubSets(leetcode - 90)
     3. Permutation (leetcode - 46)
+    4. String Permutation
