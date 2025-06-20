45216bdec5d60febd0d82079b2e6c085f5d47475
vsaa-org <23mcce16@uohyd.ac.in>
2025-06-20 22:23:41 +0530

    Quick Sort

---
+++ day15/QuickSort.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int partition(vector<int>& arr, int l, int r)
+{
+    int idx = l-1, pivot = arr[r];
+    for(int i=l;i<r;i++)
+    {
+        if(arr[i] <= pivot)
+        {
+            idx++;
+            swap(arr[i], arr[idx]);
+        }
+    }
+    idx++;
+    swap(arr[idx], arr[r]);
+    return idx;
+}
+
+void quickSort(vector<int>& arr, int l, int r)
+{
+    if(l<r)
+    {
+        int pivot = partition(arr, l, r);
+        quickSort(arr, l, pivot-1);
+        quickSort(arr, pivot+1, r);
+    }
+}
+
+int main()
+{
+    vector<int> arr = {32, 12, 5, 32, 11, 7, 9};
+
+    quickSort(arr, 0, arr.size()-1);
+
+    for(int ele : arr)
+    {
+        cout << ele << " ";
+    }
+    cout << endl;
+    return 0;
+}
--- day15/readme.txt
+++ day15/readme.txt
@@ -1,3 +1,5 @@
 Day 15:
     1. Combination Sum (leetcode - 39).
     2. Palindrome Partition (leetcode - 131).
+    3. Merge Sort.
+    4. Quick Sort.
