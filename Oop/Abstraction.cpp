e6fd9209006a3a037f378146e983095b8401a6cc
Anuj Dangi <23mcce16@uohyd.ac.in>
2025-06-28 23:45:37 +0530

    Abstraction in c++

--- Oop/Abstraction.cpp
+++ Oop/Abstraction.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+//Abstraction
+//Hiding all unnecessary details & showing only the important parts
+//by using access modifiers and using abstract classes
+//abstract class
+class Shape
+{
+public:
+    virtual void draw() = 0;    //pure virtual function
+};
+
+class Circle : public Shape
+{
+public:
+    void draw()
+    {
+        cout << "drawing a circle" << endl;
+    }
+};
 
 int main()
 {
+    Circle c;
+    c.draw();
     return 0;
 }
