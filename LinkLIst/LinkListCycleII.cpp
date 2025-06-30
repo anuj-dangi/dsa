b5123354217630c15d7400d47f77e918684b1767
Anuj Dangi <23mcce16@uohyd.ac.in>
2025-06-30 23:02:27 +0530

    LinkListCycleII(leetcode - 142)

---
+++ LinkLIst/LinkListCycleII.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+//leetcode - 142
+//using slow-fast approach
+ListNode *detectCycle(ListNode *head)
+{
+    ListNode *slow = head;
+    ListNode *fast = head;
+
+    while (fast && fast->next)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+
+        if (fast == slow)
+        {
+            slow = head;
+            while (fast != slow)
+            {
+                fast = fast->next;
+                slow = slow->next;
+            }
+
+            return slow;
+        }
+    }
+
+    return NULL;
+}
+
+int main()
+{
+    ListNode a(7);
+    ListNode temp(2, &a);
+    ListNode head(5, &temp);
+    a.next = &head;
+    cout << detectCycle(&head);
+    return 0;
+}
