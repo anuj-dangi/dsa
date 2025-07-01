7dfb5f332b5950d434455572a95e5d0d56b65440
Anuj Dangi <23mcce16@uohyd.ac.in>
2025-07-01 13:39:12 +0530

    Merge two sorted linked list (leetcode - 21)

--- LinkLIst/MergeTwoSortedLL.cpp
+++ LinkLIst/MergeTwoSortedLL.cpp
@@ -58,29 +58,45 @@ ListNode *mergeTwoLists(ListNode *list1, ListNode *list2)
 ListNode *merge(ListNode *list1, ListNode *list2)
 {
 
-    if (list1 && list2)
+    if (list1 == NULL || list2 == NULL)
     {
-        if (list1->val <= list2->val)
-        {
-            list1->next = merge(list1->next, list2);
-            return list1;
-        }
-        else
-        {
-            list2->next = merge(list1, list2->next);
-            return list2;
-        }
+        return list1 == NULL ? list2 : list1;
     }
-    else if (list1)
+
+    if (list1->val <= list2->val)
     {
+        list1->next = merge(list1->next, list2);
         return list1;
     }
-    else if (list2)
+    else
     {
+        list2->next = merge(list1, list2->next);
         return list2;
     }
-    else
-        return NULL;
+
+    // if (list1 && list2)
+    // {
+    //     if (list1->val <= list2->val)
+    //     {
+    //         list1->next = merge(list1->next, list2);
+    //         return list1;
+    //     }
+    //     else
+    //     {
+    //         list2->next = merge(list1, list2->next);
+    //         return list2;
+    //     }
+    // }
+    // else if (list1)
+    // {
+    //     return list1;
+    // }
+    // else if (list2)
+    // {
+    //     return list2;
+    // }
+    // else
+    //     return NULL;
 }
 
 int main()
