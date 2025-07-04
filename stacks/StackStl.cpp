9f95dd55be1052922cfca7db8c6fc858d1694747
Anuj Dangi <23mcce16@uohyd.ac.in>
2025-07-04 09:18:19 +0530

    Stack using STL

---
+++ stacks/StackStl.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main()
+{
+    stack<int> s;
+
+    s.push(10);
+    s.push(20);
+    s.push(30);
+
+    while(!s.empty())
+    {
+        cout << s.top() << " ";
+        s.pop();
+    }
+    cout << endl;   //30 20 10
+    return 0;
+}
