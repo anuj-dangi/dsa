d00b54312ed54dd2394264f33b5c27107cd6dec3
Anuj Dangi <23mcce16@uohyd.ac.in>
2025-07-06 10:28:44 +0530

    Updated Next greater element II

--- stacks/NextGreaterElementII.cpp
+++ stacks/NextGreaterElementII.cpp
@@ -6,21 +6,34 @@ using namespace std;
 //leetcode - 503
 vector<int> nextGreater(vector<int> nums)
 {
-    vector<int> ans(nums.size(), -1);
+    int n = nums.size();
+    vector<int> ans(n, -1);
     stack<int> s;
-    for(int i=nums.size()-2;i>=0;i--)
+    // for(int i=nums.size()-2;i>=0;i--)
+    // {
+    //     s.push(nums[i]);
+    // }
+    // for(int i=nums.size()-1;i>=0;i--)
+    // {
+    //     while(!s.empty() && s.top() <= nums[i])
+    //     {
+    //         s.pop();
+    //     }
+
+    //     ans[i] = s.empty() ? -1 : s.top();
+    //     s.push(nums[i]);
+    // }
+
+    //another approach
+    for(int i=2*nums.size()-1;i>=0;i--)
     {
-        s.push(nums[i]);
-    }
-    for(int i=nums.size()-1;i>=0;i--)
-    {
-        while(!s.empty() && s.top() <= nums[i])
+        while(!s.empty() && nums[s.top()] <= nums[i%n])
         {
             s.pop();
         }
 
-        ans[i] = s.empty() ? -1 : s.top();
-        s.push(nums[i]);
+        ans[i%n] = s.empty() ? -1 : nums[s.top()];
+        s.push(i%n);
     }
 
     return ans;
