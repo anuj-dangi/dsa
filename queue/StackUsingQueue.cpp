cf8a0390548b068b5aa6cced3aa011c19719c727
Anuj Dangi <23mcce16@uohyd.ac.in>
2025-07-11 22:15:32 +0530

    stack using only one  queue

--- queue/StackUsingQueue.cpp
+++ queue/StackUsingQueue.cpp
@@ -3,69 +3,112 @@
 #include <queue>
 using namespace std;
 
+//leetcode - 225
+//using two queue
+// class SUQ
+// {
+//     queue<int> q1;
+//     queue<int> q2;
+// public:
+
+//     SUQ()
+//     {
+//         //do nothing
+//     }
+
+//     void push(int data)
+//     {
+//         q1.push(data);
+//     }
+
+//     int pop()
+//     {
+//         int ele;
+
+//         while(!q1.empty())
+//         {
+//             ele = q1.front();
+//             q1.pop();
+
+//             if(!q1.empty())
+//                 q2.push(ele);
+//         }
+
+//         q1.swap(q2);
+        
+//         while(!q2.empty())
+//         {
+//             q2.pop();
+//         }
+
+//         return ele;
+//     }
+
+//     bool empty()
+//     {
+//         return q1.empty();
+//     }
+
+//     int top()
+//     {
+//         int ele;
+
+//         while(!q1.empty())
+//         {
+//             ele = q1.front();
+//             q1.pop();
+//             q2.push(ele);
+//         }
+
+//         q1.swap(q2);
+//         while(!q2.empty())
+//         {
+//             q2.pop();
+//         }
+//         return ele;
+//     }
+
+// };
+
+//using only one queue
 class SUQ
 {
-    queue<int> q1;
-    queue<int> q2;
-public:
+    queue<int> q;
 
+public:
     SUQ()
     {
         //do nothing
     }
 
+    bool empty()
+    {
+        return q.empty();
+    }
+
     void push(int data)
     {
-        q1.push(data);
+        int size = q.size();
+        q.push(data);
+
+        for(int i=0;i<size;i++)
+        {
+            q.push(q.front());
+            q.pop();
+        }
     }
 
     int pop()
     {
-        int ele;
-
-        while(!q1.empty())
-        {
-            ele = q1.front();
-            q1.pop();
-
-            if(!q1.empty())
-                q2.push(ele);
-        }
-
-        q1.swap(q2);
-        
-        while(!q2.empty())
-        {
-            q2.pop();
-        }
-
-        return ele;
-    }
-
-    bool empty()
-    {
-        return q1.empty();
+        int data = q.front();
+        q.pop();
+        return data;
     }
 
     int top()
     {
-        int ele;
-
-        while(!q1.empty())
-        {
-            ele = q1.front();
-            q1.pop();
-            q2.push(ele);
-        }
-
-        q1.swap(q2);
-        while(!q2.empty())
-        {
-            q2.pop();
-        }
-        return ele;
+        return q.front();
     }
-
 };
 
 int main()
