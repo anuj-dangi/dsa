ee3589f4f9e513a4529c2c438b56e511ff56bb33
Anuj Dangi <23mcce16@uohyd.ac.in>
2025-07-26 20:55:11 +0530

    Longest Consecutive Sequence (leetcode - 128)

--- day19/LongestConsecutiveSequence.cpp
+++ day19/LongestConsecutiveSequence.cpp
@@ -1,40 +1,65 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <unordered_set>
 using namespace std;
 
-//leetcode - 128
+// leetcode - 128
 int longestConsecutive(vector<int> &nums)
 {
-    if (nums.size() == 0)
-        return 0;
-    sort(nums.begin(), nums.end()); //avg - nlogn
+     if (nums.size() == 0)
+         return 0;
+    // sort(nums.begin(), nums.end()); //avg - nlogn
 
-    int count = 0, len = 0;
+    // int count = 0, len = 0;
+
+    // //O(n)
+    // for (int i = nums.size() - 1; i >= 1; i--)
+    // {
+    //     if (nums[i] - nums[i - 1] == 1)
+    //     {
+    //         len++;
+    //     }
+    //     else if (nums[i] == nums[i - 1])
+    //         continue;
+    //     else
+    //         len = 0;
+
+    //     count = max(count, len);
+    // }
+
+    // return count + 1;
 
     //O(n)
-    for (int i = nums.size() - 1; i >= 1; i--)
+    unordered_set<int> numSet(nums.begin(), nums.end());
+    int maxLength = 0;
+    for (int num : numSet)
     {
-        if (nums[i] - nums[i - 1] == 1)
+        // Only start counting from the beginning of a sequence
+        if (numSet.find(num - 1) == numSet.end())
         {
-            len++;
+            int currentNum = num;
+            int currentLength = 1;
+            while (numSet.find(currentNum + 1) != numSet.end())
+            {
+                currentNum++;
+                currentLength++;
+                
+            }
+            maxLength = max(maxLength, currentLength);
+            
         }
-        else if (nums[i] == nums[i - 1])
-            continue;
-        else
-            len = 0;
-
-        count = max(count, len);
+        
     }
-
-    return count + 1;
+    return maxLength;
+    
 }
 
 int main()
 {
-    vector<int> nums = {100,4,200,1,3,2};
+    vector<int> nums = {100, 4, 200, 1, 3, 2};
 
     cout << longestConsecutive(nums) << endl;
-    
+
     return 0;
 }
